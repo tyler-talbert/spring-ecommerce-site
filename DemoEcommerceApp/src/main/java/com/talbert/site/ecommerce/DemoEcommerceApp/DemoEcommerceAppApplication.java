53c892d3be9c6e64d48244d7e2349f702e89a5f3
anonymous-cloud-192 <Sparklecleansoutheast@gmail.com>
2025-02-23 16:58:58 -0500

    Initial commit -- Spring configuration

---
+++ DemoEcommerceApp/src/main/java/com/talbert/site/ecommerce/DemoEcommerceApp/DemoEcommerceAppApplication.java
@@ -0,0 +1,13 @@
+package com.talbert.site.ecommerce.DemoEcommerceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEcommerceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEcommerceAppApplication.class, args);
+	}
+
+}
---
+++ DemoEcommerceApp/src/test/java/com/talbert/site/ecommerce/DemoEcommerceApp/DemoEcommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talbert.site.ecommerce.DemoEcommerceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEcommerceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
